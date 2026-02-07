956fe830e74c12cf6184521820f6de0210992fbf
Nityagopaljena98 <nityagjena@hotmail.com>
2026-02-08 04:17:12 +0530

    feat(login): implement secure OTP generation

--- server/models/otp.js
+++ server/models/otp.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 
 const otpSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
     otp: { type: String, required: true },
     expiresAt: { type: Date, required: true },
     attempts: { type: Number, default: 0 }
