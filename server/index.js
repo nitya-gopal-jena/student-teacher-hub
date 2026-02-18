04e99d9a791035bb9c5593b7a84a16719e53e37f
Nityagopaljena98 <nityagjena@hotmail.com>
2026-02-18 08:25:06 +0530

    Configured backend to allow cross-origin requests

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 import { configDotenv } from 'dotenv';
 import connectDB from './config/db.js';
 import router from './routes/studentRouter.js';
+import cors from 'cors';
 
 const app = express();
 configDotenv();
 
 // Middlewares
 app.use(express.json());
+
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 // Add router path for student
 app.use('/api/students', router);
