8e41e2543b7f2d428363942cddaf0351c05a3974
Nityagopaljena98 <nityagjena@hotmail.com>
2026-02-21 12:18:37 +0530

    Implement logout functionality to navbar after login

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../css/navbar.css'
+import { toast } from 'react-toastify';
+import '../css/navbar.css';
 
 const Navbar = () => {
- 
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
     navigate('/login');
-  }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  },);
+
+  const handleLogOut = () => {
+    localStorage.removeItem('jwt');
+    setIsLoggedIn(false);
+    toast.success('Logout Successfully');
+    navigate('/login');
+  };
 
   return (
     <>
@@ -23,8 +38,16 @@ const Navbar = () => {
           <Link to='/contact'>Contact</Link>
         </div>
 
-        <div className="login-btn">
-          <button type='submit' className='login'  onClick={handleLogin} >Login</button>
+        <div className='login-btn'>
+          {!isLoggedIn ? (
+            <button type='submit' className='login' onClick={handleLogin}>
+              Login
+            </button>
+          ) : (
+            <button type='submit' className='logout' onClick={handleLogOut}>
+              Logout
+            </button>
+          )}
         </div>
       </div>
     </>
